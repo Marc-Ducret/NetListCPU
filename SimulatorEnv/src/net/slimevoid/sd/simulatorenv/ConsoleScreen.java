a3cb9e1435e62e90a3ea4c9e098514bd1d7bde6a
Marc Ducret <mducret@clipper.ens.fr>
2017-01-13 12:07:06 +0100

    new screen framework

---
+++ SimulatorEnv/src/net/slimevoid/sd/simulatorenv/ConsoleScreen.java
@@ -0,0 +1,27 @@
+package net.slimevoid.sd.simulatorenv;
+
+public class ConsoleScreen implements Screen {
+
+	private int w, h;
+
+	public void init(int w, int h) {
+		this.w = w;
+		this.h = h;
+	}
+
+	public void draw(char[] buff) {
+		StringBuilder builder = new StringBuilder();
+		for(int y = 0; y < h; y ++) {
+			for(int x = 0; x < w; x ++) {
+				builder.append(buff[x+w*y]);
+			}
+			builder.append('\n');
+		}
+		System.out.print(builder.toString());
+	}
+
+	public void clear() {
+		System.out.print("\033[H\033[2J");  
+		System.out.flush(); 
+	}
+}
